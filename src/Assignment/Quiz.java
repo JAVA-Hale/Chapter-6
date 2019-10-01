181bb8b4a32536a229be6ab59c3e6a91c3c447dd
Thomas hale <TH146688@davenportschools.org>
2019-10-01 08:41:45 -0500

    update change

---
+++ src/Assignment/Quiz.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+        int choice;
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many legs does a spider have?  \n 1-for 7 " +
+                "\n 2- for 8 \n 3- for 4 \n 4- for 10 \n choice >>>>");
+        choice = input.nextInt();
+
+        if (choice == 1) {
+
+
+        }
+    }
+}
